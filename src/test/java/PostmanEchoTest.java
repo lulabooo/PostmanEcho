c11cfc7688a70c1211fd3bab08ac0efc0181be5a
Maria Loginova <marialog@yandex.ru>
2023-05-04 18:40:52 +0300

    third

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,10 +1,10 @@
+
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
 public class PostmanEchoTest {
     @Test
     public void shouldPostRawTextTest() {
